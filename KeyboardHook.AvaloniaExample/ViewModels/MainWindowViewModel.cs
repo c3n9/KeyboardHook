4e256536557510758700f9d19eebbd67a181e425
Manakov Arseniy <108518693+c3n9@users.noreply.github.com>
2025-11-17 16:27:25 +0300

    Added example for Avalonia

---
+++ KeyboardHook.AvaloniaExample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KeyboardHook.Interfaces;
+
+namespace KeyboardHook.AvaloniaExample.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string _keyDownStr;
+
+        [ObservableProperty]
+        private string _keyUpStr;
+
+        public MainWindowViewModel()
+        {
+            IKeyboardHook keyboardHook = KeyboardHookFactory.Create();
+
+            keyboardHook.KeyDown += KeyboardHook_KeyDown;
+            keyboardHook.KeyUp += KeyboardHook_KeyUp; ;
+        }
+
+        private void KeyboardHook_KeyUp(Enums.KeyboardKey key)
+        {
+            KeyUpStr = key.ToString();
+        }
+
+        private void KeyboardHook_KeyDown(Enums.KeyboardKey key)
+        {
+            KeyDownStr = key.ToString();
+        }
+    }
+}
---
+++ KeyboardHook.AvaloniaExample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KeyboardHook.AvaloniaExample.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
